7270ddf157a3c6eff3fcdb2d9aea3c2c93c234ca
mdadnanmalik8523 <69727717+mdadnanmalik8523@users.noreply.github.com>
2025-04-22 02:06:29 +0530

    Update SnapHack.py

--- SnapHack.py
+++ SnapHack.py
@@ -6,4 +6,4 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 
-def snapchat_brute(username, password): session = requests.Session() login_url = 'https://accounts.snapchat.com/login'
+def snapchat_brute(username, password): session = requests.Session() login_url = 'https://accounts.snapchat.com/accounts/v2/login'
